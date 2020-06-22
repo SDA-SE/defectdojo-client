0c86f4c0b89f723dbe3e466870b7779f735056fb
Timo Pagel <github@timo-pagel.de>
2020-06-22 12:09:44 +0200

    int conversion of prodType

--- defectdojo.groovy
+++ defectdojo.groovy
@@ -21,7 +21,7 @@ String dojoUrl = System.getenv("DD_URL") ?: "http://localhost:8080"
 String reportPath = System.getenv("DD_REPORT_PATH") ?: "/dependency-check-report.xml"
 String reportType = System.getenv("DD_REPORT_TYPE") ?: "Dependency Check Scan"
 String importType = System.getenv("DD_IMPORT_TYPE") ?: "import" // reimport
-String productType = System.getenv("DD_PRODUCT_TYPE") ?: 1
+int productType = System.getenv("DD_PRODUCT_TYPE").toInteger() ?: 1
 String branchName = System.getenv("DD_BRANCH_NAME")
 if(!branchName) {
   println "Error: No branchName"
