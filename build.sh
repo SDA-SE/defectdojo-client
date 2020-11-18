73d9a54f11bc30fd7d0bf4e1455ca5e5cdaf10d6
Timo Pagel <github@timo-pagel.de>
2020-11-18 11:22:09 +0100

    Change version

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ chown -R 999:999 "${defectdojo_mnt}/code/.groovy"
 
 echo "defectdojo:x:999:999:OWASP DefectDojo,,,:/code:/usr/sbin/nologin" >> ${defectdojo_mnt}/etc/passwd
 
-version=1.0.18
+version=1.0.20
 oci_prefix="org.opencontainers.image"
 buildah config \
   --label "${oci_prefix}.authors=SDA SE Engineers <engineers@sda-se.io>" \
