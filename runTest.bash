cd8a5b469a948cb0d840235e0bb4d0dd9d1bacf4
Timo Pagel <git@timo-pagel.de>
2021-11-04 11:19:05 +0100

    Flip order of test

--- runTest.bash
+++ runTest.bash
@@ -10,7 +10,7 @@ export DD_URL="https://defectdojo-test.tools.sda-se.io/"
 #export DD_URL=http://localhost:8081/
 
 export DD_USER="admin"
-export DD_PRODUCT_NAME="test-2021-03-22-dedup-on-engagement-level-engagement-per-image"
+export DD_PRODUCT_NAME="test-2021-11-04"
 export DD_PRODUCT_DESCRIPTION="Test defectdojo by tpagel"
 export DD_BRANCH_NAME="image:2.0.0"
 #export DD_BUILD_ID=1
@@ -26,14 +26,14 @@ export DD_REPORT_PATH="./test/findings.csv"
 export DD_REPORT_TYPE="Generic Findings Import"
 groovy defectdojo.groovy
 groovy defectdojo.groovy # two times
-exit 0
+
 
 export DD_REPORT_TYPE="Dependency Check Scan"
 export DD_BRANCH_NAME="image:2.0.1"
-export DD_REPORT_PATH="./test/dependency-check-report-5.xml"
+export DD_REPORT_PATH="./test/dependency-check-report-10.xml"
 groovy defectdojo.groovy
 
-export DD_REPORT_PATH="./test/dependency-check-report-10.xml"
+export DD_REPORT_PATH="./test/dependency-check-report-5.xml"
 groovy defectdojo.groovy
 
 export DD_REPORT_PATH="./test/findings.csv"
