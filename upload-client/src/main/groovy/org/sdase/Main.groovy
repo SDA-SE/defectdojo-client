062b83b19698c23ee44af654b6bf5593cd178e11
Timo Pagel <github@timo-pagel.de>
2022-08-16 07:47:34 +0200

    use prod template

--- upload-client/src/main/groovy/org/sdase/Main.groovy
+++ upload-client/src/main/groovy/org/sdase/Main.groovy
@@ -8,6 +8,18 @@ public class Main {
         GroovyObject importToDefectDojo = (GroovyObject) groovyClass.newInstance();
 */
         String productName = System.getenv("DD_PRODUCT_NAME")
+        String productTemplate = System.getenv("DD_PRODUCT_NAME")
+        if(productName == null && productTemplate != null) {
+            productName=productTemplate
+                    .replace("###NAMESPACE###", System.getenv("ENVIRONMENT"))
+                    .replace("###NAMESPACE###", System.getenv("NAMESPACE"))
+                    .replace("###APP_NAME###", System.getenv("APP_NAME"))
+            if(productName.contains("###")) {
+                println "Error: productName ${productName} still contains template variables"
+                exit 3
+            }
+        }
+
         if(!productName) {
             println "Error: No productName"
             return
