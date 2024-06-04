098c7d38459ceba80ddf46aa17f28eaabcc90ba8
Timo Pagel <github@timo-pagel.de>
2024-06-04 12:14:18 +0200

    fix: Illegal Argument

--- upload-client/src/main/groovy/org/sdase/deleteTestProduct/MainDeleteProduct.groovy
+++ upload-client/src/main/groovy/org/sdase/deleteTestProduct/MainDeleteProduct.groovy
@@ -2,24 +2,6 @@ package org.sdase.deleteTestProduct
 
 public class MainDeleteProduct {
     static void main(String[] args) {
-        String dojoUrl = System.getenv("DEFECTDOJO_URL")
-        if (dojoUrl == null) {
-            dojoUrl = System.getenv("DD_URL")
-        }
-        println "dojoUrl: ${dojoUrl}"
-        String token = System.getenv("DEFECTDOJO_APIKEY")
-        if(token == null) {
-            token = System.getenv("DD_TOKEN")
-        }
-        String dojoUser = System.getenv("DEFECTDOJO_USERNAME")
-        if(dojoUser == null) {
-            dojoUser = System.getenv("DD_USER")
-        }
-        if(!token) {
-            println "Error: No token"
-            return
-        }
-
-        org.sdase.deleteTestProduct.TestProductDeletor.main (token, dojoUser, dojoUrl)
+        org.sdase.deleteTestProduct.TestProductDeletor.call ()
     }
 }
